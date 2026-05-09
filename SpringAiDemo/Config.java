2f35f3ef881e1da73457d71f2d7f9c35f6dc22d9
Sakshi Thorat <159508795+sakshisantoshthorat@users.noreply.github.com>
2026-05-09 20:57:38 +0530

    Add files via upload

---
+++ SpringAiDemo/Config.java
@@ -0,0 +1,15 @@
+package com.ai.SpringAiDemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public ChatClient chatClient(ChatModel chatModel) {
+        return ChatClient.create(chatModel);
+    }
+}
