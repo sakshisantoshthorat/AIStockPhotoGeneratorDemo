190707f99304b2091e335c58ef0c9078cdc86c9e
Sakshi Thorat <159508795+sakshisantoshthorat@users.noreply.github.com>
2026-05-09 20:58:01 +0530

    Add files via upload

---
+++ SpringAiDemo/GenAiController.java
@@ -0,0 +1,75 @@
+package com.ai.SpringAiDemo;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.ai.image.ImageResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+public class GenAiController {
+
+    private final ChatService chatService;
+    private final ImageService imageService;
+    private final RecipeService recipeService;
+
+    public GenAiController(ChatService chatService, ImageService imageService, RecipeService recipeService) {
+
+        this.chatService = chatService;
+        this.imageService = imageService;
+        this.recipeService = recipeService;
+    }
+    @GetMapping("/")
+    public String home() {
+        return "Application Running";
+    }
+
+    @GetMapping("/ask-ai")
+    public String getResponse(@RequestParam String prompt){
+        return chatService.getResponse(prompt);
+    }
+
+    @GetMapping("ask-ai-options")
+    public String getResponseOptions(@RequestParam String prompt){
+        return chatService.getResponseOptions(prompt);
+    }
+
+//    @GetMapping("generate-images")
+//    public void generateImages(HttpServletResponse response, @RequestParam String prompt){
+//        ImageResponse imageResponse = imageService.generateImage(prompt);
+//        String imageUrl = imageResponse.getResult().getOutput().getUrl();
+//        try {
+//            response.sendRedirect(imageUrl);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+
+    @GetMapping("generate-images")
+    public List<String> generateImages(HttpServletResponse response, @RequestParam String prompt,
+                                       @RequestParam(defaultValue = "hd") String quality,
+                                       @RequestParam(defaultValue = "1") int n,
+                                       @RequestParam(defaultValue = "1024") int width,
+                                       @RequestParam(defaultValue = "1024") int height) throws IOException {
+        ImageResponse imageResponse = imageService.generateImage(prompt,quality,n,width,height);
+        //Streams to get urls from ImageResponse
+
+        List<String> imageUrls = imageResponse.getResults().stream()
+                .map(result -> result.getOutput().getUrl())
+                .toList();
+
+        return imageUrls;
+    }
+
+    @GetMapping("recipe-creator")
+    public String recipeCreator(@RequestParam String ingredients,
+                                      @RequestParam(defaultValue = "any") String cuisine,
+                                      @RequestParam(defaultValue = "") String dietaryRestrictions
+                                      ){
+        return recipeService.createRecipe(ingredients,cuisine,dietaryRestrictions);
+    }
+}
+
